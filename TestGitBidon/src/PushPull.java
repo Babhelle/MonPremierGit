c36de511eb4ccefd7b390f6f65a16383010886dd
Babou <Babou@Isidore>
2016-09-15 20:03:16 +0200

    Tadaaaaaaaaaaaaaaaaaaaaam !

---
+++ TestGitBidon/src/PushPull.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Babou
+ *
+ */
+public class PushPull {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * c'est juste comme ça ! pour la gloire ^^
+		 */
+	}
+
+}
